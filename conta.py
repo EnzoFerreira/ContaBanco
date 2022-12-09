95ea25e2c1739785bfd716ba4d1a9c578233bdaa
EnzoFerreira <enzoraci21@gmail.com>
2022-12-09 11:35:52 -0300

    itens estáticos e outros métodos adicionados na conta

--- conta.py
+++ conta.py
@@ -1,3 +1,4 @@
+
 class Conta:
     def __init__(self, numero, titular, saldo, limite):
         print(f'Construindo objeto{self}')
@@ -6,6 +7,8 @@ class Conta:
         self.__saldo = saldo
         self.__limite = limite
 
+#----------------------------Métodos-------------------------------
+
     def extrato(self):
         print(f'{self.__saldo} do  {self.__titular}')
 
@@ -13,19 +16,33 @@ class Conta:
         self.__saldo += valor
 
     def sacar(self, valor):
-        self.__saldo -= valor
+        if self.__podeSacar(valor):
+            self.__saldo -= valor
+        else:
+            print(f'Você não está autorizado a sacar este valor!')
 
     def tranferir(self, valor, destino):
         self.sacar(valor)
         destino.depositar(valor)
+#------------------------------------------------------------------
+    def __podeSacar(self,valorSacar):
+        valorDisponivel = self.__saldo + self.__limite
+        return valorSacar <= valorDisponivel
 
-    def get_saldo(self):
+    @staticmethod #ST Codigo banco
+    # #static n precisa de objeto p funcionar
+    def codigos_Bancos():
+        return {'BB':'001', 'Caixa':'104', 'Bradesco': '237'}
+
+    @property # GT Saldo
+    def saldo(self):
         return self.__saldo
 
-    def get_titular(self):
+    @property #GT Titular
+    def titular(self):
         return self.__titular
 
-    @property
+    @property # GT limite
     def limite(self):
         return self.__limite
 
